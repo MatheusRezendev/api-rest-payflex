c253be008d98ad2b879bb8316be5b34e10c70cda
Matheus Reginato Rezende <contatorzn@hotmail.com>
2025-02-20 15:00:15 -0300

    structure-entities

---
+++ src/entities/user.ts
@@ -0,0 +1,59 @@
+import { UserType } from "./userType";
+
+export type UserProps = {
+    id: string;
+    nome: string;
+    email: string;
+    cpf?: string;
+    cnpj?: string;
+    senha: string;
+    tipo: UserType;
+}
+
+export class User {
+    private constructor(readonly props: UserProps) {}
+
+    public static build(nome: string, email: string, senha: string, tipo: UserType, cpf?: string, cnpj?: string) {
+        // Validação de cpf e cnpj
+
+        return new User({
+            id: crypto.randomUUID().toString(),
+            nome,
+            email,
+            senha,
+            tipo,
+            cpf,
+            cnpj
+        }) 
+    }
+
+    public podeTransferir() {
+        return this.props.tipo === UserType.usuario;
+    }
+
+    public get id() {
+        return this.props.id;
+    }
+
+    public get nome() {
+        return this.props.nome;
+    }
+
+    public get email() {
+        return this.props.email;
+    }
+
+    public get cpf() {
+        return this.props.cpf;
+    }
+
+    public get cnpj() {
+        return this.props.cnpj;
+    }
+
+    public get tipo() {
+        return this.props.tipo;
+    }
+
+
+}
---
+++ src/entities/userType.ts
@@ -0,0 +1,5 @@
+export enum UserType {
+    usuario = "usuario",
+    lojista = "lojista"
+}
+
