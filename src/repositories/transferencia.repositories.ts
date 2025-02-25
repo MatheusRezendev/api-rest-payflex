6fa6cfe76f1b571292d8dfe2d1a0643265b052fa
MatheusRezendev <contatorzn@hotmail.com>
2025-02-25 17:40:17 -0300

    feat: add implementation transferencia

--- src/repositories/transferencia.repositories.ts
+++ src/repositories/transferencia.repositories.ts
@@ -2,7 +2,7 @@ import { Transferencia } from "../entities/transferencia";
 
 export interface TransferenciaRepository {
     save(transferencia: Transferencia): Promise<void>;
-    findById(id: string): Promise<void>;
+    findById(id: string): Promise<Transferencia | null>;
     listByUserId(userId: string): Promise<Transferencia[]>;
     updateStatus(id: string, status: string): Promise<void>;
 }
